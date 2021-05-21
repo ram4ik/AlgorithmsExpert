fe50df1740fb61556411975d74ec0d0355a60b02
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-21 14:56:27 +0300

    Initial Commit

---
+++ AlgorithmsExpert/AlgorithmsExpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsExpertApp.swift
+//  AlgorithmsExpert
+//
+//  Created by ramil on 21.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsExpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
